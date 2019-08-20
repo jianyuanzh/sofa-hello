b749d57a3fdb6929afacebff9a925f7562af5d10
Jianyuan <jianyuan@databus.cc>
2019-08-20 23:38:49 +0800

    init with spring boot

---
+++ src/main/java/cc/databus/cyclops/CyclopsApplication.java
@@ -0,0 +1,13 @@
+package cc.databus.cyclops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyclopsApplication.class, args);
+    }
+
+}
